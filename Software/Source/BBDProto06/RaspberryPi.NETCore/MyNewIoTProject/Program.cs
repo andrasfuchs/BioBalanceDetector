1fadf55e7d9177174ae6f19e5ac0f27b0509d666
Andras Fuchs <andras.fuchs@gmail.com>
2019-03-30 00:43:12 +0100

    Raspberry Pi .NET Core samples

---
+++ Software/Source/BBDProto06/RaspberryPi.NETCore/MyNewIoTProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mynewiotproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Internet of Things!");
+        }
+    }
+}
