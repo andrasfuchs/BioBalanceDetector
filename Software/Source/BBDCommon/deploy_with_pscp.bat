c28df4d897d230d4ef1b628f89d77a8504a9173e
Andras Fuchs <andras.fuchs@gmail.com>
2019-05-15 16:40:53 +0200

    Two batch files to make remote building for Raspberry Pi easier

---
+++ Software/Source/BBDCommon/deploy_with_pscp.bat
@@ -0,0 +1,1 @@
+"c:\Program Files\PuTTY\pscp.exe" -pw raspPi36 -p -q -r c:\Work\BioBalanceDetector\Software\Source\BBDProto06\RaspberryPi.NETCore\myHelloWorldApp\bin\Debug\netcoreapp2.2\linux-arm\publish\*.* pi@192.168.1.35:/home/pi/Work/BioBalanceDetector/Software/Source/BBDProto06/RaspberryPi.NETCore/myHelloWorldApp/bin/Debug/netcoreapp2.2/linux-arm/publish/
