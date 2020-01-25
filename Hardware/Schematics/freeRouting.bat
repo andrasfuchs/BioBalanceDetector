241537d9e1e2d623d49b4f038df16013d0698f6f
Andras Fuchs <andras.fuchs@gmail.com>
2020-01-25 10:16:04 +0100

    Updated the autorounter

--- Hardware/Schematics/freeRouting.bat
+++ Hardware/Schematics/freeRouting.bat
@@ -1,1 +1,4 @@
-"c:\Work\Tools\jdk-11.0.1\bin\javaw.exe" -jar "c:\Program Files (x86)\LayoutEditor\bin\freeRouting.jar"
+@ECHO OFF
+ECHO if not set already, set the JAVA_HOME environment variable to the shortened Java installation folder (eg. "C:\Progra~1\Java\jdk-11.0.6")
+
+"%JAVA_HOME%\bin\javaw.exe" -jar "c:\Work\BioBalanceDetector\Tools\KiCad_FreeRouting\FreeRouting-miho-master\freerouting-master\build\libs\freerouting-executable.jar"
