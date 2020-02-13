2da36115606cf54af6fa26f78039003036768d7d
Andras Fuchs <andras.fuchs@gmail.com>
2020-02-13 16:12:52 +0100

    Autorouted Mars-64 rev B

--- Hardware/Schematics/BBDProto07/Mars-64/Autorouter-revB/autorouter.bat
+++ Hardware/Schematics/BBDProto07/Mars-64/Autorouter-revB/autorouter.bat
@@ -4,6 +4,6 @@ GOTO :eof
 
 :execute
 echo Starting autorouter for '%~n1'
-start cmd.exe /K "c:\Progra~1\Java\jdk-11.0.6\bin\java.exe -jar freerouting-executable.jar -de %~n1.dsn -do %~n1.ses -mp 100"
-timeout 3
+start cmd.exe /K "c:\Progra~1\Java\jdk-11.0.6\bin\java.exe -jar freerouting-executable.jar -de %~n1.dsn -do %~n1.ses -dr Mars-64-revB.rules -mp 100"
+REM timeout 3
 GOTO :eof
