c72e00120d173ad3afab24c634cb8c7d1399a7bf
Andras Fuchs <andras.fuchs@gmail.com>
2020-02-16 18:26:12 +0100

    Created a Raspberry Pi .NET Core publisher PowerShell script

---
+++ Software/Source/BBDProto07/RaspberryPi.NETCore/BBD_Mars-64/Mars-64/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mars_64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.Write("Waiting for debugger to attach... ");
+            while (true)
+            {
+                Console.Write(++i + " ");
+                if (System.Diagnostics.Debugger.IsAttached) break; 
+                System.Threading.Thread.Sleep(1000); 
+            }
+            Console.WriteLine();
+        }
+    }
+}
