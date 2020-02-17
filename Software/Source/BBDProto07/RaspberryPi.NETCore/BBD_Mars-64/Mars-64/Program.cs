f97dd7179b4f9fe285ccf01ef4828b4b2c6b6964
Andras Fuchs <andras.fuchs@gmail.com>
2020-02-17 14:34:54 +0100

    Updated the Mars-64 sample app

--- Software/Source/BBDProto07/RaspberryPi.NETCore/BBD_Mars-64/Mars-64/Program.cs
+++ Software/Source/BBDProto07/RaspberryPi.NETCore/BBD_Mars-64/Mars-64/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Unosquare.RaspberryIO;
+using Unosquare.WiringPi;
 
 namespace Mars_64
 {
@@ -6,6 +8,16 @@ namespace Mars_64
     {
         static void Main(string[] args)
         {
+            if (string.IsNullOrWhiteSpace(Pi.Info.OperatingSystem.ToString()))
+            {
+                Console.WriteLine("ERROR: You must run this app on a Raspberry Pi");
+                return;
+            }
+
+            Console.WriteLine("Raspberry Pi OS: " + Pi.Info.OperatingSystem);
+
+            Pi.Init<BootstrapWiringPi>();
+
             int i = 0;
             Console.Write("Waiting for debugger to attach... ");
             while (true)
