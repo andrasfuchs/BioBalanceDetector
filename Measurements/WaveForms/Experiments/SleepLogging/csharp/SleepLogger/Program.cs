8e42894f3bce60c966246d703ae1c187803925b1
Andras Fuchs <andras.fuchs@gmail.com>
2021-01-07 19:37:00 +0100

    Fix the DllImport reference to the DWF libary binary

--- Measurements/WaveForms/Experiments/SleepLogging/csharp/SleepLogger/Program.cs
+++ Measurements/WaveForms/Experiments/SleepLogging/csharp/SleepLogger/Program.cs
@@ -43,45 +43,45 @@ namespace SleepLogger
 
 		static void Main(string[] args)
 		{
-			dwf.FDwfGetVersion(out string dwfVersion);
+			Dwf.FDwfGetVersion(out string dwfVersion);
 			Console.WriteLine($"DWF Version: {dwfVersion}");
 
 			Console.WriteLine("Opening first device");
-			dwf.FDwfDeviceOpen(-1, out int dwfHandle);
+			Dwf.FDwfDeviceOpen(-1, out int dwfHandle);
 
-			if (dwfHandle == dwf.hdwfNone)
+			if (dwfHandle == Dwf.hdwfNone)
 			{
-				dwf.FDwfGetLastErrorMsg(out string lastError);
+				Dwf.FDwfGetLastErrorMsg(out string lastError);
 				Console.WriteLine($"Failed to open device: {lastError}");
 				return;
 			}
 
-			dwf.FDwfAnalogInBufferSizeInfo(dwfHandle, out int bufferSizeMinimum, out int bufferSizeMaximum);
+			Dwf.FDwfAnalogInBufferSizeInfo(dwfHandle, out int bufferSizeMinimum, out int bufferSizeMaximum);
 			bufferSize = Math.Min(bufferSizeMaximum, samplerate);
 			Console.WriteLine($"Device buffer size range: {bufferSizeMinimum} - {bufferSizeMaximum} samples, set to {bufferSize}.");
 			voltData = new double[bufferSize];
 
 			//set up acquisition
-			dwf.FDwfAnalogInFrequencySet(dwfHandle, samplerate);
-			dwf.FDwfAnalogInBufferSizeSet(dwfHandle, bufferSize);
-			dwf.FDwfAnalogInChannelEnableSet(dwfHandle, 0, 1);
-			dwf.FDwfAnalogInChannelRangeSet(dwfHandle, 0, 5.0);
+			Dwf.FDwfAnalogInFrequencySet(dwfHandle, samplerate);
+			Dwf.FDwfAnalogInBufferSizeSet(dwfHandle, bufferSize);
+			Dwf.FDwfAnalogInChannelEnableSet(dwfHandle, 0, 1);
+			Dwf.FDwfAnalogInChannelRangeSet(dwfHandle, 0, 5.0);
 
 			// set up signal generation
-			dwf.FDwfAnalogOutNodeEnableSet(dwfHandle, signalGeneratorChannel, dwf.AnalogOutNodeCarrier, 1);            
-			dwf.FDwfAnalogOutNodeFunctionSet(dwfHandle, signalGeneratorChannel, dwf.AnalogOutNodeCarrier, dwf.funcSine);
-			dwf.FDwfAnalogOutNodeFrequencySet(dwfHandle, signalGeneratorChannel, dwf.AnalogOutNodeCarrier, signalGeneratorFrequency);
-			dwf.FDwfAnalogOutNodeAmplitudeSet(dwfHandle, signalGeneratorChannel, dwf.AnalogOutNodeCarrier, signalGeneratorAmplitude);
+			Dwf.FDwfAnalogOutNodeEnableSet(dwfHandle, signalGeneratorChannel, Dwf.AnalogOutNodeCarrier, 1);            
+			Dwf.FDwfAnalogOutNodeFunctionSet(dwfHandle, signalGeneratorChannel, Dwf.AnalogOutNodeCarrier, Dwf.funcSine);
+			Dwf.FDwfAnalogOutNodeFrequencySet(dwfHandle, signalGeneratorChannel, Dwf.AnalogOutNodeCarrier, signalGeneratorFrequency);
+			Dwf.FDwfAnalogOutNodeAmplitudeSet(dwfHandle, signalGeneratorChannel, Dwf.AnalogOutNodeCarrier, signalGeneratorAmplitude);
 
 			Console.WriteLine($"Generating sine wave @{signalGeneratorFrequency}Hz...");
-			dwf.FDwfAnalogOutConfigure(dwfHandle, signalGeneratorChannel, 1);
+			Dwf.FDwfAnalogOutConfigure(dwfHandle, signalGeneratorChannel, 1);
 
 			//wait at least 2 seconds for the offset to stabilize
 			Thread.Sleep(2000);
 
 			//start aquisition
 			Console.WriteLine("Starting oscilloscope");
-			dwf.FDwfAnalogInConfigure(dwfHandle, 0, 1);
+			Dwf.FDwfAnalogInConfigure(dwfHandle, 0, 1);
 
 			Console.WriteLine($"Recording data @{samplerate}Hz, press Ctrl+C to stop...");
 
@@ -91,16 +91,16 @@ namespace SleepLogger
 			{
 				while (true)
 				{
-					dwf.FDwfAnalogInStatus(dwfHandle, 1, out byte sts);
+					Dwf.FDwfAnalogInStatus(dwfHandle, 1, out byte sts);
 
-					if (sts == dwf.DwfStateDone)
+					if (sts == Dwf.DwfStateDone)
 						break;
 
 					Thread.Sleep(100);
 				}
 
 
-				dwf.FDwfAnalogInStatusData(dwfHandle, 0, voltData, bufferSize);     //get channel 1 data CH1
+				Dwf.FDwfAnalogInStatusData(dwfHandle, 0, voltData, bufferSize);     //get channel 1 data CH1
 				samples.AddRange(voltData.Select(vd => (float)vd));
 				Console.Write(".");
 
@@ -138,7 +138,7 @@ namespace SleepLogger
 			Console.WriteLine();
 			Console.WriteLine("Acquisition done");
 
-			dwf.FDwfDeviceCloseAll();
+			Dwf.FDwfDeviceCloseAll();
 		}
 
 		private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
